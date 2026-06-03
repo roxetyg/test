7f023fa073c305af90bc2b2b0306c49de01a867b
roxetyg <amine.lemnouar1@gmail.com>
2026-06-03 14:07:30 +0100

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "The Aithēr Studios bot",
+        name: "The Aithēr Studios",
         // Activity type number (0 = Playing).
-        type: 4, 
+        type: 0, 
       },
     ],
   },
